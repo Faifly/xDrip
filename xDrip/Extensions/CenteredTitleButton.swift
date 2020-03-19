70ac87834a86bc0a84cb1b562a41a20335673a6f
Ivan Skoryk <>
2020-03-19 14:49:57 +0200

    Fixed code style. Reduced tab bar view height. Added tab bar border view.

--- xDrip/Extensions/CenteredTitleButton.swift
+++ xDrip/Extensions/CenteredTitleButton.swift
@@ -8,22 +8,30 @@
 
 import UIKit
 
-class CenteredTitleButton: UIButton {
+final class CenteredTitleButton: UIButton {
     
     let padding: CGFloat = 4.0
     
     override func titleRect(forContentRect contentRect: CGRect) -> CGRect {
         let rect = super.titleRect(forContentRect: contentRect)
 
-        return CGRect(x: 0, y: contentRect.height - rect.height,
-            width: contentRect.width, height: rect.height)
+        return CGRect(
+            x: 0,
+            y: contentRect.height - rect.height,
+            width: contentRect.width,
+            height: rect.height
+        )
     }
 
     override func imageRect(forContentRect contentRect: CGRect) -> CGRect {
         let rect = super.imageRect(forContentRect: contentRect)
 
-        return CGRect(x: contentRect.width/2.0 - rect.width/2.0, y: 0,
-            width: rect.width, height: rect.height)
+        return CGRect(
+            x: contentRect.width / 2.0 - rect.width / 2.0,
+            y: 0,
+            width: rect.width,
+            height: rect.height
+        )
     }
 
     override var intrinsicContentSize: CGSize {
@@ -31,12 +39,20 @@ class CenteredTitleButton: UIButton {
 
         if let image = imageView?.image {
             var labelHeight: CGFloat = 0.0
+            
+            let contentSize = CGSize(
+                width: self.contentRect(forBounds: self.bounds).width,
+                height: CGFloat.greatestFiniteMagnitude
+            )
 
-            if let size = titleLabel?.sizeThatFits(CGSize(width: self.contentRect(forBounds: self.bounds).width, height: CGFloat.greatestFiniteMagnitude)) {
+            if let size = titleLabel?.sizeThatFits(contentSize) {
                 labelHeight = size.height
             }
 
-            return CGSize(width: max(titleLabel?.frame.width ?? 0.0, image.size.width), height: image.size.height + labelHeight + padding)
+            return CGSize(
+                width: max(titleLabel?.frame.width ?? 0.0, image.size.width),
+                height: image.size.height + labelHeight + padding
+            )
         }
 
         return size
