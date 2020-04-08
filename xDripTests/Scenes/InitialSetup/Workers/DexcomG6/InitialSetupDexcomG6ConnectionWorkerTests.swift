3cdc371721c22b0c81054c7f03a5119b2756c4a5
Ivan Skoryk <>
2020-04-08 12:39:48 +0300

    Add unit test to initial setup workers.

--- xDripTests/Scenes/InitialSetup/Workers/DexcomG6/InitialSetupDexcomG6ConnectionWorkerTests.swift
+++ xDripTests/Scenes/InitialSetup/Workers/DexcomG6/InitialSetupDexcomG6ConnectionWorkerTests.swift
@@ -7,6 +7,45 @@
 //
 
 import XCTest
+@testable import xDrip
 
 final class InitialSetupDexcomG6ConnectionWorkerTests: XCTestCase {
+    
+    let sut = InitialSetupDexcomG6ConnectionWorker()
+    var calledSuccessfulConnectionHandler = false
+    
+    override func setUp() {
+        super.setUp()
+        
+        sut.onSuccessfulConnection = { _ in
+            self.calledSuccessfulConnectionHandler = true
+        }
+    }
+    
+    func testStartConnection() {
+        // When
+        sut.startConnectionProcess()
+        
+        // Then
+        XCTAssertTrue(CGMDevice.current.isSetupInProgress)
+        XCTAssertTrue(CGMController.shared.service is DexcomG6BluetoothService)
+    }
+    
+    func testHandleSuccessfulConnection() {
+        // When
+        sut.startConnectionProcess()
+        
+        let controller = CGMController.shared
+        controller.serviceDidUpdateMetadata(.firmwareVersion, value: "123")
+        XCTAssert(calledSuccessfulConnectionHandler == false)
+        
+        controller.serviceDidUpdateMetadata(.batteryVoltageA, value: "90")
+        XCTAssert(calledSuccessfulConnectionHandler == false)
+        
+        controller.serviceDidUpdateMetadata(.batteryVoltageB, value: "78")
+        XCTAssert(calledSuccessfulConnectionHandler == false)
+        
+        controller.serviceDidUpdateMetadata(.transmitterTime, value: "123123123")
+        XCTAssert(calledSuccessfulConnectionHandler == true)
+    }
 }
--- xDripTests/Scenes/InitialSetup/Workers/DexcomG6/InitialSetupDexcomG6IDSavingWorkerTests.swift
+++ xDripTests/Scenes/InitialSetup/Workers/DexcomG6/InitialSetupDexcomG6IDSavingWorkerTests.swift
@@ -10,4 +10,58 @@ import XCTest
 @testable import xDrip
 
 final class InitialSetupDexcomG6IDSavingWorkerTests: XCTestCase {
+    
+    let sut = InitialSetupDexcomG6IDSavingWorker()
+    
+    func testValidate() {
+        // When
+        var id: String? = nil
+        // Then
+        XCTAssert(sut.validate(id) == false)
+        
+        // When
+        id = "abcdef"
+        // Then
+        XCTAssert(sut.validate(id) == false)
+        
+        // When
+        id = "123456"
+        // Then
+        XCTAssert(sut.validate(id) == true)
+        
+        // When
+        id = "ABC123"
+        // Then
+        XCTAssert(sut.validate(id) == true)
+        
+        // When
+        id = "ABCDEF"
+        // Then
+        XCTAssert(sut.validate(id) == true)
+        
+        // When
+        id = "1234567"
+        // Then
+        XCTAssert(sut.validate(id) == false)
+        
+        // When
+        id = "12345"
+        // Then
+        XCTAssert(sut.validate(id) == false)
+    }
+    
+    func testSave() {
+        let id = "123456"
+        
+        guard sut.validate(id) else {
+            XCTFail("ID not valid")
+            return
+        }
+        
+        // When
+        sut.saveID(id)
+        
+        // Then
+        XCTAssert(CGMDevice.current.metadata(ofType: .serialNumber) != nil)
+    }
 }
