a693b03fd1ea2a76846f0223f64617740258058a
Ivan Skoryk <ivan.skoryk@icloud.com>
2020-08-18 10:52:53 +0300

    Add logs to Nightscout networking. (#69)
    * Add logs to Nightscout networking.
    
    * Add more logs.
    
    Co-authored-by: Ivan Skoryk <>

---
+++ xDrip/Extensions/Data+String.swift
@@ -0,0 +1,19 @@
+//
+//  Data+String.swift
+//  xDrip
+//
+//  Created by Ivan Skoryk on 13.08.2020.
+//  Copyright © 2020 Faifly. All rights reserved.
+//
+
+import Foundation
+
+extension Data {
+    var prettyPrintedJSONString: String? {
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+              let prettyPrintedString = String(data: data, encoding: .utf8) else { return nil }
+
+        return prettyPrintedString
+    }
+}
