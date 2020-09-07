9a147ce5a3f73e7c71183ff5bf6090112744ede5
Dmitriy Podnebennyi <dmitriy.podnebennyi@faifly.com>
2020-09-07 16:17:43 +0300

    Add unit test for OptionView

--- xDrip/Scenes/Home/Home/Views/OptionsView.swift
+++ xDrip/Scenes/Home/Home/Views/OptionsView.swift
@@ -24,8 +24,6 @@ enum Option: Int {
 
 final class OptionsView: NibView {
     @IBOutlet private weak var contentView: UIView!
-    @IBOutlet private weak var allTrainingsGestureRecognizer: UITapGestureRecognizer!
-    @IBOutlet private weak var allBasalsGestureRecognizer: UITapGestureRecognizer!
     @IBOutlet private weak var allTrainingsTilteLabel: UILabel!
     @IBOutlet private weak var allBasalsTitleLabel: UILabel!
     @IBOutlet private weak var allTrainingsContentView: UIView!
@@ -38,31 +36,20 @@ final class OptionsView: NibView {
         allTrainingsTilteLabel.text = Option.allTrainings.title
         allBasalsTitleLabel.text = Option.allBasals.title
         
-        allTrainingsGestureRecognizer.addTarget(self, action: #selector (self.onAllTrainingsTapped))
-        allBasalsGestureRecognizer.addTarget(self, action: #selector (self.onAllBasalTapped))
+        let allTrainingsGestureRecognizer = UITapGestureRecognizer(target: self,
+                                                                   action: #selector(onAllTrainingsTapped(sender:)))
+        let allBasalsGestureRecognizer = UITapGestureRecognizer(target: self,
+                                                                action: #selector(onAllBasalTapped(sender:)))
+        
+        allTrainingsContentView.addGestureRecognizer(allTrainingsGestureRecognizer)
+        allBasalsContentView.addGestureRecognizer(allBasalsGestureRecognizer)
     }
     
     @objc func onAllTrainingsTapped(sender: UITapGestureRecognizer) {
-        highliteView(allTrainingsContentView)
         itemSelectionHandler?(.allTrainings)
     }
     
     @objc func onAllBasalTapped(sender: UITapGestureRecognizer) {
-        highliteView(allBasalsContentView)
         itemSelectionHandler?(.allBasals)
-    }
-    
-    private func highliteView(_ view: UIView) {
-        let bgcolor = view.backgroundColor
-        UIView.animate(withDuration: 0.1,
-                       delay: 0,
-                       options: [.allowUserInteraction, .curveLinear],
-                       animations: {
-                        view.backgroundColor = .borderColor
-        }, completion: { _ in
-            UIView.animate(withDuration: 0.1, delay: 0, options: [.allowUserInteraction, .curveLinear], animations: {
-                view.backgroundColor = bgcolor
-            })
-        })
     }
 }
