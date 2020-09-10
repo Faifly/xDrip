abede8ad5524120022c251e4444b109c32265273
Dmitriy Podnebennyi <dmitriy.podnebennyi@faifly.com>
2020-09-10 14:01:17 +0300

    Remove unnecessary check for existence before treatments deletion

--- xDrip/Services/CloudSync/NightscoutService.swift
+++ xDrip/Services/CloudSync/NightscoutService.swift
@@ -268,28 +268,12 @@ final class NightscoutService {
             
             if !requestQueue.contains(where: { $0.itemID == entry.externalID && $0.type == deleteRequestType
             }), let uuid = entry.externalID {
-                guard let findRequest = requestFactory.createFindTreatmentRequest(uuid: uuid) else { return }
-                URLSession.shared.loggableDataTask(with: findRequest) { data, _, error in
-                    guard let data = data, error == nil else { return }
-                    guard let entries = try? JSONDecoder().decode([CTreatment].self, from: data) else { return }
-                    if !entries.isEmpty {
-                        guard let request = self.requestFactory.createDeleteTreatmentRequest(uuid,
-                                                                                             requestType: deleteRequestType) else {
-                                                                                                return
-                        }
-                        self.requestQueue.append(request)
-                        self.runQueue()
-                    } else {
-                        switch treatmentType {
-                        case .carbs:
-                            CarbEntriesWorker.deleteEntryWith(externalID: uuid)
-                        case .bolus, .basal:
-                            InsulinEntriesWorker.deleteEntryWith(externalID: uuid)
-                        case .training:
-                            TrainingEntriesWorker.deleteEntryWith(externalID: uuid)
-                        }
-                    }
-                }.resume()
+                guard let request = self.requestFactory.createDeleteTreatmentRequest(uuid,
+                                                                                     requestType: deleteRequestType) else {
+                                                                                        return
+                }
+                self.requestQueue.append(request)
+                self.runQueue()
             }
         }
     }
