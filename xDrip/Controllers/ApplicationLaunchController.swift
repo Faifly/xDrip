b00e9dedff378adcf1e9dd881560bd38a1d4c72e
Ivan Skoryk <>
2020-10-23 14:02:58 +0300

    Increment Realm schema version.

--- xDrip/Controllers/ApplicationLaunchController.swift
+++ xDrip/Controllers/ApplicationLaunchController.swift
@@ -37,7 +37,7 @@ enum ApplicationLaunchController {
     
     private static func setupRealm() {
         Realm.Configuration.defaultConfiguration = Realm.Configuration(
-            schemaVersion: 2,
+            schemaVersion: 3,
             migrationBlock: { _, _ in },
             deleteRealmIfMigrationNeeded: false
         )
